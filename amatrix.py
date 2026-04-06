532edc27c1cf081eeeee520767ad59b47a8790c0
bjacovazzo <94629627+bjacovazzo@users.noreply.github.com>
2026-04-06 11:14:28 -0300

    Montagem da matriz de gdl dos elementos

--- amatrix.py
+++ amatrix.py
@@ -1,6 +1,81 @@
+import sys
 import numpy as np
 
-# Local beam stiffness matrix:
+print()
+print("Program AMatrix")
+print("======= =======")
+
+# Model data:
+# -----------------------------------------------
+nodes = {
+    "A":{"coord":[0.,0.], "bc": ["fixed","fixed"],
+        "nodal_forces": [-60.,-60.]},
+    "B":{"coord":[6.,0.], "bc": ["fixed","free"],
+        "nodal_forces": [-60.,80.]},
+    "C":{"coord":[8.,0.], "bc": ["free","free"],
+        "nodal_forces": [-50.,0.]},
+    "D":{"coord":[10.,0.], "bc": ["fixed","free"],
+        "nodal_forces": [0.,0.]}
+}
+
+elems = {
+    "b1":{"ei": 45000., "ni": "A", "nf": "B",
+        "fef": [60.,60.,60.,-60.]},
+    "b2":{"ei": 45000., "ni": "B", "nf": "C",
+        "fef": [0.,0.,0.,0.]},
+    "b3":{"ei": 45000., "ni": "C", "nf": "D",
+        "fef": [0.,0.,0.,0.]}
+}
+
+nnode = len(nodes)
+nelem = len(elems)
+
+print()
+print(f"Number of nodes = {nnode}")
+print(f"Number of elements = {nelem}")
+
+# Degrees of freedom:
+# -----------------------------------------------
+ndof = 0
+ndpr = 0
+
+def build_node_dof(node):
+    global ndof, ndpr
+    dof_matrix = []
+    for dof in node["bc"]:
+        if dof == "free":
+            dof_matrix.append(ndof)
+            ndof += 1
+        elif dof == "fixed":
+            dof_matrix.append(ndpr)
+            ndpr += 1
+    node["dof"] = dof_matrix
+    node["dof_type"] = node["bc"]
+        
+for node in nodes.values():
+    build_node_dof(node)
+
+print()
+print(f"Number of degrees of freedom = {ndof}")
+print(f"Number of prescribed displacements = {ndpr}\n")
+
+# Element dof:
+# -----------------------------------------------
+def build_elem_dof(elem):
+    ni = nodes[elem["ni"]]
+    nf = nodes[elem["nf"]]
+    elem["dof"] = ni["dof"] + nf["dof"]
+    elem["dof_type"] = ni["dof_type"] + nf["dof_type"]
+
+for elem in elems.values():
+    build_elem_dof(elem)
+
+# End program
+# ===============================================
+sys.exit()
+# ===============================================
+
+# Stiffness matrix for beam element:
 # -----------------------------------------------
 def kbeam(ei,lx):
     k = np.zeros((4,4), dtype=float)
@@ -22,25 +97,24 @@ def kbeam(ei,lx):
 
 # Problem data
 # -----------------------------------------------
-eis = np.array([
-    45000,
-    45000,
-    45000
-], dtype=float)
+# eis = np.array([
+#     45000,
+#     45000,
+#     45000
+# ], dtype=float)
 
-lxs = np.array([
-    6,
-    2,
-    2
-], dtype=float)
+# lxs = np.array([
+#     6,
+#     2,
+#     2
+# ], dtype=float)
 
-elem_dof = np.array([
-    [1,2,3,4],
-    [3,4,5,6],
-    [5,6,7,8]
-], dtype=int)
+# elem_dof = np.array([
+#     [1,2,3,4],
+#     [3,4,5,6],
+#     [5,6,7,8]
+# ], dtype=int)
 
-nelem = len(eis)
 
 # Global stiffness matrix
 # -----------------------------------------------
@@ -71,6 +145,15 @@ kgg_ff = kgg_re[0:4,0:4].copy()
 kgg_fp= kgg_re[0:4,4:8].copy()
 kgg_pf = kgg_re[4:8,0:4].copy()
 kgg_pp = kgg_re[4:8,4:8].copy()
+
+# print()
+# print(f"kgg_ff =\n{kgg_ff}")
+# print()
+# print(f"kgg_fp =\n{kgg_fp}")
+# print()
+# print(f"kgg_pf =\n{kgg_pf}")
+# print()
+# print(f"kgg_pp =\n{kgg_pp}")
 
 # Displacements:
 # -----------------------------------------------
